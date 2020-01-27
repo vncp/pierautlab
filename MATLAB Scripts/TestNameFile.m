60c900c68f62839fbda40b97002a5a5ac277549e
vncp <vpham@nevada.unr.edu>
2020-01-26 20:46:18 -0800

    Deleted all folders

--- MATLAB Scripts/TestNameFile.m
+++
@@ -1,5 +0,0 @@
-Files.Stim.File{1} = '/Users/vince/lab/pierautlab/SimilarityAnalysisTest/StimInput/13 001-C1.axgx';
-Files.Resp.File{1}{1} = '/Users/vince/lab/pierautlab/SimilarityAnalysisTest/RespInput/PatSep_R0.75_30Hz 008.axgd';
-Files.Resp.File{1}{2} = '/Users/vince/lab/pierautlab/SimilarityAnalysisTest/RespInput/PatSep_R0.75_SFrange_PoissonFR 009.axgd';
-Vrest{1}{1} = -71; Rm{1}{1} = 200; Cm{1}{1} = 31; ID{1}{1} = 1;
-Vrest{1}{2} = -75; Rm{1}{2} = 200; Cm{1}{2} = 40; ID{1}{2} = 2;
